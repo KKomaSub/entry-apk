2bd3bb87da4871ece14f3ccf6abdf50e84b8af0d
KKoma Programmer <ijunu3343@gmail.com>
2026-02-16 16:41:47 +0900

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kkoma.entryoffline',
+  appName: '엔트리 오프라인',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
